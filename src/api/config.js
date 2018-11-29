88de2963d50c215c94ab7bf17d6fa3af2ee4ec93
MichaelSornoza <42742012+MichaelSornoza@users.noreply.github.com>
2018-11-29 17:52:28 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,13 @@
+/*Application name	PlatziMusic
+API key	cb6ea31229ef628d0db016b029ec9171
+Shared secret	6a9236ae7a64da4269d94f78af5d5135
+Registered to	MichaelSornoza*/
+
+export default {
+
+  appname: 'PlatziMusic',
+  apiKey: 'cb6ea31229ef628d0db016b029ec9171',
+  secret: '6a9236ae7a64da4269d94f78af5d5135',
+  registereTo: 'MichaelSornoza'
+
+}
